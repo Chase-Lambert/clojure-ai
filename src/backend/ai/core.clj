bd26e1a1106c894df3c2ee8f9d113da6ffbd004f
Chase Lambert <chaselambert@gmail.com>
2021-09-10 16:14:26 -0500

    added ring reitit to backend

--- src/backend/ai/core.clj
+++ src/backend/ai/core.clj
@@ -1,4 +1,17 @@
 (ns ai.core
+  (:require [reitit.ring :as ring]
+            [ring.adapter.jetty :as jetty]
+            [clojure.java.io :as io])
   (:gen-class))
 
-(defn -main [])
+(defn index-handler [_]
+  {:body (slurp (io/resource "public/index.html"))})
+
+(def app 
+  (ring/ring-handler
+    (ring/router
+      [["/"] {:get index-handler}])))
+
+(defn -main [& _]
+  (jetty/run-jetty #'app {:port 8910}))
+  
